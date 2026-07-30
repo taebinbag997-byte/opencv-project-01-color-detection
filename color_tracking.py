078c477cb3fa39d75a1b0f8ae1cb9ba3b6c0cd57
taebinbag997-byte <taebinbag997@gmail.com>
2026-07-30 17:59:10 +0900

    Update color_tracking.py

--- color_tracking.py
+++ color_tracking.py
@@ -3,6 +3,41 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+    height, width, _ = frame.shape
+    screen_center_x = width // 2
+    screen_center_y = height // 2
+    left_line_x = width // 3
+    right_line_x = (width*2) // 3 
+    top_line_y = height // 3
+    bottom_line_y = (height*2) // 3
+    cv2.line(
+    frame,
+    (left_line_x, 0),
+    (left_line_x, height),
+    (0,255,0),
+    2
+    )
+    cv2.line(
+    frame,
+    (0, top_line_y),
+    (width, top_line_y),
+    (0,255,0),
+    2
+    )
+    cv2.line(
+    frame,
+    (right_line_x, 0),
+    (right_line_x, height),
+    (0,255,0),
+    2
+    )
+    cv2.line(
+    frame,
+    (0, bottom_line_y),
+    (width, bottom_line_y),
+    (0,255,0),
+    2
+    )
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_red = (0, 120, 70)
     upper_red = (10,255, 255)
@@ -22,33 +57,103 @@ while True:
     )
     for cnt in red_contours:
         x, y, w, h = cv2.boundingRect(cnt)
+    
 
         if w * h > 1000:
-            cv2.rectangle(
+            red_center_x = x + w//2
+            red_center_y = y + h//2
+            error_x = screen_center_x - red_center_x
+            error_y = screen_center_y - red_center_y
+            cv2.circle(
                 frame,
-                (x, y),
-                (x + w, y + h),
-                (0, 0, 255),
-                2
+                (red_center_x, red_center_y),
+                5,
+                (0,255,0),
+                -1
             )
+           
+            if red_center_x < left_line_x:
+                cv2.putText(frame, "X : LEFT", 
+                    (50,150),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,255,0),
+                    2)
+
+            elif left_line_x < red_center_x < right_line_x:
+                cv2.putText(frame, "X : CENTER", 
+                    (50,150),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,255,0),
+                    2)
+            else:
+                cv2.putText(frame, "X : RIGHT", 
+                    (50,150),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,255,0),
+                    2)
+            if red_center_y < top_line_y:
+                cv2.putText(frame, "Y : UP", 
+                (50,250),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,255,0),
+                2)
+            
+            elif top_line_y < red_center_y < bottom_line_y:
+                cv2.putText(frame, "Y : CENTER", 
+                (50,250),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,255,0),
+                2)
+            else:
+                cv2.putText(frame, "Y : BOTTOM", 
+                (50,250),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,255,0),
+                2)
+
+    
     for cnt in blue_contours:
         x, y, w, h = cv2.boundingRect(cnt)
+        
 
         if w * h > 1000:
-            cv2.rectangle(
+            blue_center_x = x + w//2
+            blue_center_y = y + h//2
+            cv2.circle(
                 frame,
-                (x, y),
-                (x + w, y + h),
-                (255, 0, 0),
-                2
+                (blue_center_x,blue_center_y),
+                5,
+                (0, 255, 0),
+                -1
             )
+            # if blue_center_x < screen_center_x:
+            #     cv2.putText(frame, "LEFT", 
+            #     (50,150),
+            #     cv2.FONT_HERSHEY_SIMPLEX,
+            #     1,
+            #     (0,255,0),
+            #     2)
+            
+            # elif blue_center_x > screen_center_x:
+            #     cv2.putText(frame, "RIGHT", 
+            #     (40,180),
+            #     cv2.FONT_HERSHEY_SIMPLEX,
+            #     1,
+            #     (0,255,0),
+            #     2)
     red_pixels = cv2.countNonZero(mask)
     blue_pixels = cv2.countNonZero(mask2)
     if red_pixels >5000:
-        cv2.putText(frame,"RED",(50,70),cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame,"RED",(50,50),cv2.FONT_HERSHEY_SIMPLEX
                     ,1,(0,0,255),2)
     if blue_pixels >5000:
-        cv2.putText(frame,"BLUE",(80,100),cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame,"BLUE",(50,90),cv2.FONT_HERSHEY_SIMPLEX
                     ,1,(255,0,0),2)
     cv2.imshow("Camera", frame)
     cv2.imshow("Red Mask", mask)
